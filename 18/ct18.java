a69886897a5dce9a0421feee335085a78e844140
pigeonroh <30688998+pigeonroh@users.noreply.github.com>
2017-10-04 16:58:40 -0700

    first

---
+++ 18/ct18.java
@@ -0,0 +1,32 @@
+public class ct18{
+    public static void main(String[] args){
+        int[][] matrix = new int[5][4];
+
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+        matrix[0][2] = 3;
+        matrix[0][3] = 4;
+
+        matrix[1][0] = 5;
+        matrix[1][1] = 6;
+        matrix[1][2] = 7;
+        matrix[1][3] = 8;
+
+        matrix[2][0] = 9;
+        matrix[2][1] = 10;
+        matrix[2][2] = 11;
+        matrix[2][3] = 12;
+
+        matrix[3][0] = 13;
+        matrix[3][1] = 14;
+        matrix[3][2] = 15;
+        matrix[3][3] = 16;
+
+        matrix[4][0] = 17;
+        matrix[4][1] = 18;
+        matrix[4][2] = 19;
+        matrix[4][3] = 20;
+
+
+    }
+}
